d7a9759c721c23301e623e3a72f5148aa0c069c2
王萍 <wang.03.25@qq.com>
2018-01-24 12:40:34 +0800

    packing structure

---
+++ nettyPro/src/main/java/com/wp/filedownload/HttpFileServer.java
@@ -0,0 +1,51 @@
+package com.wp.filedownload;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class HttpFileServer {
+
+    //允许访问的根目录。
+    private static final String DEFAULT_URL = "/";
+
+    public void run(final int port, final String url) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch)
+                                throws Exception {
+                            ch.pipeline().addLast("http-codec", new HttpServerCodec());
+                            ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                            // 加入chunked 主要作用是支持异步发送的码流（大文件传输），但不占用过多的内存，防止java内存溢出
+                            ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                        }
+                    });
+            ChannelFuture future = b.bind(port).sync();
+            System.out.println("HTTP文件目录服务器启动，网址是 : " + "http://localhost:" + port + url);
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        String url = DEFAULT_URL;
+        new HttpFileServer().run(port, url);
+    }
+}
