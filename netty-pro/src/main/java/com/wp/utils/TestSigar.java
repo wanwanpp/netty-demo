46bd99b0ff35278a97885ccdab7b1d1eaf499a0a
王萍 <wang.03.25@qq.com>
2018-01-24 13:26:50 +0800

    packing structure

--- netty-pro/src/main/java/com/wp/utils/TestSigar.java
+++ netty-pro/src/main/java/com/wp/utils/TestSigar.java
@@ -1,24 +1,11 @@
 package com.wp.utils;
 
+import org.hyperic.sigar.*;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.Properties;
-
-import org.hyperic.sigar.Cpu;
-import org.hyperic.sigar.CpuInfo;
-import org.hyperic.sigar.CpuPerc;
-import org.hyperic.sigar.FileSystem;
-import org.hyperic.sigar.FileSystemUsage;
-import org.hyperic.sigar.Mem;
-import org.hyperic.sigar.NetFlags;
-import org.hyperic.sigar.NetInterfaceConfig;
-import org.hyperic.sigar.NetInterfaceStat;
-import org.hyperic.sigar.OperatingSystem;
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.SigarException;
-import org.hyperic.sigar.Swap;
-import org.hyperic.sigar.Who;
 
 public class TestSigar {
     public static void main(String[] args) {
